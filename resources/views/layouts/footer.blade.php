1197cfe1ffb0f13035f0f8344bc30660599c49cd
Permanasidik69 <permanasidikk06@gmail.com>
2024-10-19 13:39:04 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>POS Visval © {{ date('Y') }} || Developed by <strong><a target="_blank" href="https://visval.co.id">Visval Official Store</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
